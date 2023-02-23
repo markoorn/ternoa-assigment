68dc1c349facd1765d47907c2fb301019b44d603
Mark <mark@silverstag.io>
2023-02-23 13:12:00 +0200

    Properly add refrock script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,15 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import Script from 'next/script'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+
+  return (<>
+    <Script dangerouslySetInnerHTML={{
+      __html:  `window.referralJS = ((window.referralJS !== null && window.referralJS !== undefined) ? window.referralJS : {}); window.referralJS.scriptConfig = { parameters: { src: "//ziggy74773.referralrock.com/ReferralSdk/referral.js", transactionKey: "a03f7617-75a2-4e85-b5e4-56290cfd5da3" } }; (function(f,r,n,d,b,y){b=f.createElement(r),y=f.getElementsByTagName(r)[0];b.async=1;b.src=n+"?referrer="+encodeURIComponent(window.location.origin+window.location.pathname).replace(/[!'()*]/g,escape);b.id="RR_DIVID_V5";b.setAttribute("transactionKey",window.referralJS.scriptConfig.parameters.transactionKey);y.parentNode.insertBefore(b,y)})(document,"script",window.referralJS.scriptConfig.parameters.src);`
+    }}  />
+    <Component {...pageProps} />
+  </>);
 }
 
 export default MyApp
--- pages/index.tsx
+++ pages/index.tsx
@@ -7,6 +7,7 @@ import Header from '../components/Header';
 import Modal from '../components/Modal';
 import { ListItem } from '../types/ListItem';
 import {ChainvineClient, storeReferrer} from '@chainvine/sdk/lib';
+import Script from 'next/script'
 
 const Home: NextPage = () => {
 
@@ -97,7 +98,6 @@ const Home: NextPage = () => {
     <div>
       <Head>
         <title>NFT Gallery</title>
-        <script> window.referralJS = ((window.referralJS !== null && window.referralJS !== undefined) ? window.referralJS : {}); window.referralJS.scriptConfig = { parameters: { src: "//ziggy74773.referralrock.com/ReferralSdk/referral.js", transactionKey: "a03f7617-75a2-4e85-b5e4-56290cfd5da3" } }; (function(f,r,n,d,b,y){b=f.createElement(r),y=f.getElementsByTagName(r)[0];b.async=1;b.src=n+"?referrer="+encodeURIComponent(window.location.origin+window.location.pathname).replace(/[!'()*]/g,escape);b.id="RR_DIVID_V5";b.setAttribute("transactionKey",window.referralJS.scriptConfig.parameters.transactionKey);y.parentNode.insertBefore(b,y)})(document,"script",window.referralJS.scriptConfig.parameters.src); </script>
       </Head>
 
       <Header
