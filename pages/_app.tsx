9d421957b1e5e9e43a9640bed39481ee047a813b
Mark <mark@silverstag.io>
2023-02-23 14:07:31 +0200

    Add conversion script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -5,8 +5,26 @@ import Script from 'next/script'
 function MyApp({ Component, pageProps }: AppProps) {
 
   return (<>
-    <Script dangerouslySetInnerHTML={{
+    <Script strategy="beforeInteractive" dangerouslySetInnerHTML={{
       __html:  `window.referralJS = ((window.referralJS !== null && window.referralJS !== undefined) ? window.referralJS : {}); window.referralJS.scriptConfig = { parameters: { src: "//ziggy74773.referralrock.com/ReferralSdk/referral.js", transactionKey: "a03f7617-75a2-4e85-b5e4-56290cfd5da3" } }; (function(f,r,n,d,b,y){b=f.createElement(r),y=f.getElementsByTagName(r)[0];b.async=1;b.src=n+"?referrer="+encodeURIComponent(window.location.origin+window.location.pathname).replace(/[!'()*]/g,escape);b.id="RR_DIVID_V5";b.setAttribute("transactionKey",window.referralJS.scriptConfig.parameters.transactionKey);y.parentNode.insertBefore(b,y)})(document,"script",window.referralJS.scriptConfig.parameters.src);`
+    }}  />
+
+    <Script strategy="lazyOnload" dangerouslySetInnerHTML={{
+      __html:  `window.referralJS = ((window.referralJS !== null && window.referralJS !== undefined) ? window.referralJS : {});
+                var firstName = 'Mark';
+                var email = 'mark@referred.xyz';
+                var externalIdentifier = '123456789';
+                var amount = 100;
+                
+                window.referralJS.conversion = {
+                    debug: "false",
+                    parameters: {
+                        firstName: "Mark",
+                        email: "mark@referred.xyz",
+                        externalIdentifier: "marktest1223",
+                        amount: "100"
+                    }
+                };`
     }}  />
     <Component {...pageProps} />
   </>);
